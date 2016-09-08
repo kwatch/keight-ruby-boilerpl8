3d5e7ac5aa0effcb207a956b44e4640119991bc6
makoto kuwata <kwa@kuwata-lab.com>
2016-09-09 07:41:30 +0900

    copy 'BabyErubis::Template' class into '__init.rb'

--- __init.rb
+++ __init.rb
@@ -3,6 +3,88 @@
 require 'fileutils'
 
 
+module BabyErubis
+
+
+  class Template
+
+    def from_file(filename, encoding='utf-8')
+      input = File.open(filename, "rb:#{encoding}") {|f| f.read() }
+      compile(parse(input), filename, 1)
+      return self
+    end
+
+    def from_str(input, filename=nil, linenum=1)
+      compile(parse(input), filename, linenum)
+      return self
+    end
+
+    PATTERN = /(^[ \t]*)?<%([=\#])? ?(.*?) ?%>([ \t]*\r?\n)?/m
+
+    def compile(src, filename=nil, linenum=1)
+      @proc = eval("proc { #{src} }", empty_binding(), filename || '(eRuby)', linenum)
+      return self
+    end
+
+    def parse(input)
+      pattern = self.class.const_get(:PATTERN)
+      src = "_buf = '';"       # preamble
+      pos = 0
+      input.scan(pattern) do |lspace, ch, code, rspace|
+        match = Regexp.last_match
+        text  = input[pos, match.begin(0) - pos]
+        pos   = match.end(0)
+        src << _t(text)
+        if    ch == '='        # expression
+          src << _t(lspace) << " _buf << (#{code}).to_s;" << _t(rspace)
+        elsif ch == '#'        # comment
+          src << _t(lspace) << ("\n" * code.count("\n")) << _t(rspace)
+        elsif lspace && rspace # statement (with spaces)
+          src << "#{lspace} #{code};#{rspace}"
+        else                   # statement
+          src << _t(lspace) << "#{code};" << _t(rspace)
+        end
+      end
+      text = pos == 0 ? input : input[pos..-1]   # or $' || input
+      src << _t(text)
+      src << " _buf.to_s\n"    # postamble
+      return src
+    end
+
+    def render(context=nil)
+      ctxobj = context.nil?          ? Object.new
+             : ! context.is_a?(Hash) ? context
+             : context.each_with_object(Object.new) {|o, (k, v)| o.instance_variable_set("@#{k}", v) }
+      return ctxobj.instance_eval(&@proc)
+    end
+
+    private
+
+    def build_text(text)
+      return "" if text.nil? || text.empty?
+      return " _buf << '#{text.gsub!(/['\\]/, '\\\\\&') || text}';"
+    end
+    alias _t build_text
+
+    def empty_binding
+      return binding()
+    end
+
+  end
+  Text = Template              # for shortcut
+
+
+end
+
+
+class Boilerpl8Template < BabyErubis::Template
+
+  rexp = BabyErubis::Template::PATTERN
+  PATTERN = Regexp.compile(rexp.to_s.sub(/<%/, '\{\%').sub(/%>/, '\%\}'))
+
+end
+
+
 class Main
 
   def self.main
@@ -46,18 +128,10 @@ class Main
   end
 
   def render_template_files(filepaths, dryrun=false)
-    require 'baby_erubis'
-    require 'keight'
-    template_class = Class.new(BabyErubis::Text) do
-      rexp = BabyErubis::Text::PATTERN
-      pattern = Regexp.compile(rexp.to_s.sub(/<%/, '\{\%').sub(/%>/, '\%\}'))
-      define_method(:pattern) { pattern }
-    end
-    #
     filepaths.each do |fpath|
       next if File.directory?(fpath)
       next if fpath == '__setup.rb'
-      s = template_class.new.from_file(fpath, 'ascii-8bit').render()
+      s = Boilerpl8Template.new.from_file(fpath, 'ascii-8bit').render()
       if s != File.open(fpath, 'rb') {|f| f.read }
         File.open(fpath, 'wb') {|f| f.write(s) } unless dryrun
       end
