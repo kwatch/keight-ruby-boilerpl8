933eb09f73bf0624284555439d2218182eab1afe
makoto kuwata <kwa@kuwata-lab.com>
2016-09-18 18:03:42 +0900

    define 'My::PublicPage' and 'My::TopPage'

--- app/action.rb
+++ app/action.rb
@@ -104,3 +104,86 @@ class My::StaticPage  < My::Action
   end
 
 end
+
+
+class My::PublicPage < My::Action
+
+  ERUBY_PATH       = ['public', 'template']
+  ERUBY_LAYOUT     = :'_layout.html'
+  SCRIPT_SUFFIX    = ".app.rb"
+
+  mapping '/{urlpath:str<.*>}'  ,  :GET=>:do_render
+
+  def do_render(urlpath)
+    filepath = File.join('public', urlpath)
+    #
+    if filepath.end_with?('.eruby') || filepath.end_with?(SCRIPT_SUFFIX)
+      raise K8::HttpException.new(404)
+    end
+    #
+    if File.directory?(filepath)
+      if ! filepath.end_with?('/')
+        location = urlpath + '/'
+        qs = @req.query_string
+        location = "#{location}?#{qs}" if qs && ! qs.empty?
+        return redirect_permanently_to(location)
+      end
+      filepath = File.join(filepath, 'index.html')
+    end
+    #
+    return handle_static_file(filepath)   if File.file?(filepath)
+    #
+    template = "#{filepath}#{ERUBY_TEXT_EXT}"
+    return handle_template_file(filepath) if File.file?(template)
+    #
+    script = "#{filepath}#{SCRIPT_SUFFIX}"
+    return handle_script_file(script)     if File.file?(script)
+    #
+    script_path = find_script_file(filepath)
+    return handle_sript_file(script_path) if script_path
+    #
+    return handle_not_found(urlpath)
+  end
+
+  private
+
+  def handle_static_file(filepath)
+    return send_file(filepath)
+  end
+
+  def handle_template_file(filepath)
+    basepath = filepath.sub('public/', '')
+    suffix = File.extname(filepath)
+    case suffix
+    when '.html', '.xml', '.atom'
+      content = render_html(basepath)
+    else
+      content = render_text(basepath)
+    end
+    return content
+  end
+
+  def handle_script_file(script_path)
+    raise NotImplementedError.new("#{self.class.name}#handle_script_file(): not implemented yet.")
+  end
+
+  def find_script_file(flepath)
+    return nil
+  end
+
+  def handle_not_found(urlpath)
+    raise K8::HttpException.new(404)
+  end
+
+end
+
+
+class My::TopPage < My::PublicPage
+
+  mapping ''               , :GET=>:do_render
+
+  def do_render
+    return render_html(:index)
+  end
+
+end
