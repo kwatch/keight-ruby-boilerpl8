03a1f03490793027c5ba3e059c0da228c6bf01fb
kwatch <kwatch@gmail.com>
2018-01-10 11:45:50 +0900

    move 'app/action/hello.rb' to 'app/api/hello.rb'

--- config/urlpath_mapping.rb
+++ config/urlpath_mapping.rb
@@ -4,14 +4,14 @@ $urlpath_mapping = [
   ['/'                             , "./app/action:TopPage"],
   ['/welcome'                      , "./app/action/welcome:WelcomePage"],
   ['/api', [
-    ['/hello'                      , "./app/action/hello:HelloAPI"],
+    ['/hello'                      , "./app/api/hello:HelloAPI"],
    #['/books'                      , "./app/api/books:BooksAPI"],
    #['/books/{book_id}/comments'   , "./app/api/books:BookCommentsAPI"],
    #['/orders'                     , "./app/api/orders:OrdersAPI"],
   ]],
   ['/admin', [
    #['/books'                      , "./app/admin/books:Admin::BooksPage"],
-   #['/orders'                     , "./app/admin/orders:Admi::nOrdersPage"],
+   #['/orders'                     , "./app/admin/orders:Admin::OrdersPage"],
   ]],
   ['/static'                       , "./app/action:StaticPage"],
   [''                              , "./app/action:PublicPage"],
