98b7926112fcf17bfc7d0430891b36d3b07d7ee0
kwatch <kwatch@gmail.com>
2018-01-10 09:39:36 +0900

    define 'BabyErubisTemplateHelper' module and move template-related code to it

--- app/action.rb
+++ app/action.rb
@@ -1,43 +1,11 @@
 # -*- coding: utf-8 -*-
 
 require 'keight'
-require 'baby_erubis'
-require 'baby_erubis/renderer'
-
-
-class StrippedHtmlTemplate < BabyErubis::Html
-  def parse(input, *args)
-    ## strip spaces of indentation
-    stripped = input.gsub(/^[ \t]+</, '<')
-    return super(stripped, *args)
-  end
-end
+require './app/helper/baby_erubis_template_helper'
 
 
 class BaseAction < K8::Action
-
-  ##
-  ## Template engine (BabyErubis)
-  ##
-
-  ERUBY_PATH       = ['app/template', 'template']
-  ERUBY_LAYOUT     = :_layout
-  #ERUBY_HTML      = BabyErubis::Html
-  ERUBY_HTML       = StrippedHtmlTemplate
-  ERUBY_HTML_EXT   = '.html.eruby'
-  ERUBY_TEXT       = BabyErubis::Text
-  ERUBY_TEXT_EXT   = '.eruby'
-  ERUBY_CACHE      = {}
-
-  include BabyErubis::HtmlEscaper  # define escape()
-  include BabyErubis::Renderer     # define eruby_render_{html,text}()
-
-  protected
-
-  alias render_html eruby_render_html
-  alias render_text eruby_render_text
-
-  public
+  include BabyErubisTemplateHelper
 
   ##
   ## Error handler (or event handler)
